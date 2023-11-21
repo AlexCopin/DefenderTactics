80579a46f50224844e3aa216d7057dbbae7da577
AlexCopin <alex.copin@outlook.com>
2023-11-21 09:13:03 +0100

    Rounds pass with enemy death

--- Source/DefenderTactics/DefenderTactics.Build.cs
+++ Source/DefenderTactics/DefenderTactics.Build.cs
@@ -8,7 +8,7 @@ public class DefenderTactics : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PopcornFX" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
