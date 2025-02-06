f514f598b5c06d026faef8a16962c96fec44dbb3
AlexCopin <alex.copin@outlook.com>
2025-02-06 18:49:48 +0100

    Remove PopcornFX

--- Source/TheProtectorate/TheProtectorate.Build.cs
+++ Source/TheProtectorate/TheProtectorate.Build.cs
@@ -8,7 +8,7 @@ public class TheProtectorate : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PopcornFX" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
